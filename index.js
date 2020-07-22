b4f3e16410da52b663ce6cb942238b51840a4f9e
Janak Shah <janak.shah@cuvva.com>
2020-07-22 11:10:04 +0100

    use payload after initialisation

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
-    console.log(`Author is ${payload.sender.login}`)
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`Author is ${payload.sender.login}`)
     console.log(`The event payload: ${payload}`);
 
 } catch (error) {
