53825a105c7fb47d5c3e2331aaed79b2bf5f3849
Janak Shah <janak.shah@cuvva.com>
2020-07-22 15:31:36 +0100

    use input values

--- index.js
+++ index.js
@@ -2,38 +2,44 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
-try {
+(async () => {
 
-    const gSheetsServiceEmail = core.getInput('gSheetsServiceEmail');
-    const gSheetsServiceKey = core.getInput('gSheetsServiceKey');
-    const spreadsheetId = core.getInput('spreadsheetId');
-    const worksheetName = core.getInput('worksheetName');
+    try {
 
-    //console.log(`Hello ${worksheetName}`);
+        const spreadsheetId = core.getInput('spreadsheetId');
+        const linguistInput = {"Ruby": "75.21%", "Dockerfile": "19.80%", "Shell": "5.00%"}
+        const worksheetIndex = core.getInput('worksheetIndex');
 
-    const doc = new GoogleSpreadsheet(spreadsheetId);
+        const doc = new GoogleSpreadsheet(spreadsheetId);
 
-    doc.useServiceAccountAuth({
-        client_email: gSheetsServiceEmail, //process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        private_key: gSheetsServiceKey //process.env.GOOGLE_PRIVATE_KEY,
-    });
+        await doc.useServiceAccountAuth({
+            client_email: env.GSHEET_CLIENT_EMAIL,
+            private_key: env.GSHEET_PRIVATE_KEY,
+        });
 
-    doc.loadInfo();
-    console.log(doc.title);
+        await doc.loadInfo();
+        console.log(doc.title);
 
-    const sheet = doc.sheetsByIndex[0];
-    console.log(sheet.title);
-    console.log(sheet.rowCount);
-    console.log(sheet.payload)
+        var languagesArray = [];
+        for (var language in linguistInput) {
+            languagesArray.push(linguistInput[language] + " - " + language);
+        }
 
-    // const time = (new Date()).toTimeString();
-    // core.setOutput("time", time);
+        
 
-    // Get the GitHub webhook payload for the GitHub event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`Author is ${github.context.payload.sender.login}`)
-    // console.log(`The event payload: ${payload}`);
+        languagesArray.unshift(github.context.payload.push?.commits[0]?.message || "No commit name", (new Date()).toUTCString())
 
-} catch (error) {
-    core.setFailed(error.message);
-}
+        console.log(languagesArray)
+
+        const sheet = doc.sheetsByIndex[worksheetIndex];
+        sheet.addRow(languagesArray);
+
+        // Get the GitHub webhook payload for the GitHub event that triggered the workflow
+        // const payload = JSON.stringify(github.context.payload, undefined, 2)
+        // console.log(`The event payload: ${payload}`);
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+
+})();
