b866ebb07c8985740855cccfe188b2876f78a97e
Janak Shah <janak.shah@cuvva.com>
2020-07-22 11:25:35 +0100

    specify new inputs

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const github = require('@actions/github');
 
 try {
 
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
+    const gSheetsServiceEmail = core.getInput('gSheetsServiceEmail');
+    const gSheetsServiceKey = core.getInput('gSheetsServiceKey');
+    const spreadsheetId = core.getInput('spreadsheetId');
+    const worksheetName = core.getInput('worksheetName');
+
+    console.log(`Hello ${gSheetsServiceEmail}`);
+    console.log(`Hello ${gSheetsServiceKey}`);
+    console.log(`Hello ${spreadsheetId}`);
+    console.log(`Hello ${worksheetName}`);
+
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`Author is ${payload.sender.login}`)
+    console.log(`Author is ${github.context.payload.sender.login}`)
     console.log(`The event payload: ${payload}`);
 
 } catch (error) {
